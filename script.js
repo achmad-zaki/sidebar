72cf6b92bd4ba4cd12da085d6eab48968365486d
ahmad zaki <achmadzaki10d@gmail.com>
2023-04-14 10:43:48 +0700

    first commit

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-const body = document.querySelector('body')
-const switchButton = document.querySelector('.toggle-switch')
-const toggleDarkMode = document.querySelector('.switch')
-const toggleClose = document.querySelector('.toggle-icon')
-const sidebar = document.querySelector('.sidebar')
+const sliderToggle = document.querySelector('.switch-button')
+const sidebarClose = document.querySelector('.sidebar')
+const navbarItem = document.getElementsByClassName('navbar-item')
+const textLink = document.getElementsByClassName('text-link')
+const brandLogo = document.querySelector('.brand-logo')
+const arrowIcon = document.querySelector('.arrow-icon')
+const nameAccount = document.querySelector('.name-account')
+const imgAvatar = document.querySelector('.img-avatar')
+const navItem = document.getElementsByClassName('nav-item')
+const activeClass = document.getElementsByClassName('nav-link')
 
-switchButton.addEventListener('click', () => (
-    body.classList.toggle('dark')
-))
-
-switchButton.addEventListener('click', () => (
-    toggleDarkMode.classList.toggle('dark')
-))
-
-toggleClose.addEventListener('click', () => (
-    sidebar.classList.toggle('close')
-))
-
-toggleClose.addEventListener('click', () => (
-    toggleClose.classList.toggle('close')
-))
-
-const navbarItem = document.getElementById('navbar-item')
-const navLink = navbarItem.getElementsByClassName('nav-link')
-for(let i = 0; i < navLink.length; i++) {
-    navLink[i].addEventListener('click', function() {
-        const current = document.getElementsByClassName("active")
-        current[0].className = current[0].className.replace(" active", "")
-        this.className += " active"
+for (let i = 0; i < activeClass.length; i++) {
+    activeClass[i].addEventListener('click', function() {
+        const current = document.getElementsByClassName('active');
+        current[0].className = current[0].className.replace(' active', '')
+        this.className += ' active'
     })
+}
+
+sliderToggle.addEventListener('click', () => {
+    sliderToggle.classList.toggle('light')
+})
+
+const sideBar = () => {
+    sidebarClose.classList.toggle('close')
+    brandLogo.classList.toggle('close')
+    for (let i = 0; i < navbarItem.length; i++) {
+        navbarItem[i].classList.toggle('close')
+    }
+    for (let i = 0; i < textLink.length; i++) {
+        textLink[i].classList.toggle('close')
+    }
+    arrowIcon.classList.toggle('close')
+    nameAccount.classList.toggle('close')
+    imgAvatar.classList.toggle('close')
 }
